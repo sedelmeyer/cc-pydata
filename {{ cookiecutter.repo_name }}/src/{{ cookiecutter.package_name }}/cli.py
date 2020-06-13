2b4ab9290986f550686379dfefb64334765c15e5
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-13 15:51:49 -0400

    src(cli): reformat docstring

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/cli.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/cli.py
@@ -1,14 +1,23 @@
 """
-Module that contains the command line app.
-Why does this file exist, and why not put this in __main__?
-  You might be tempted to import things from __main__ later, but that will cause
-  problems: the code will get executed twice:
-  - When you run `python -m {{cookiecutter.package_name}}` python will execute
-    ``__main__.py`` as a script. That means there won't be any
-    ``{{cookiecutter.package_name}}.__main__`` in ``sys.modules``.
-  - When you import __main__ it will get executed again (as a module) because
-    there's no ``{{cookiecutter.package_name}}.__main__`` in ``sys.modules``.
-  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
+{{cookiecutter.package_name}}.cli
+~~~~~~~~~~~~~
+
+This module that contains the command line app for ``{{cookiecutter.package_name}}``.
+
+
+Why does this file exist, and why not put this in ``__main__``?
+
+You might be tempted to import things from ``__main__`` later, but that will
+cause problems: the code will get executed twice:
+
+* When you run ``python -m caproj``, python will execute ``__main__.py`` as a
+  script. That means there won't be any ``caproj.__main__`` in ``sys.modules``.
+
+* When you ``import __main__`` it will get executed again (as a module) because
+  there's no ``caproj.__main__`` in ``sys.modules``.
+
+* Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
+
 """
 import argparse
 
