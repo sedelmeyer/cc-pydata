6b5c5bb45fd464cf428f459f454027e2fca3991a
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-14 08:27:27 -0400

    src(cli): fix docstring package-name templating call

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/cli.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/cli.py
@@ -10,11 +10,11 @@ Why does this file exist, and why not put this in ``__main__``?
 You might be tempted to import things from ``__main__`` later, but that will
 cause problems: the code will get executed twice:
 
-* When you run ``python -m caproj``, python will execute ``__main__.py`` as a
-  script. That means there won't be any ``caproj.__main__`` in ``sys.modules``.
+* When you run ``python -m {{cookiecutter.package_name}}``, python will execute ``__main__.py`` as a
+  script. That means there won't be any ``{{cookiecutter.package_name}}.__main__`` in ``sys.modules``.
 
 * When you ``import __main__`` it will get executed again (as a module) because
-  there's no ``caproj.__main__`` in ``sys.modules``.
+  there's no ``{{cookiecutter.package_name}}.__main__`` in ``sys.modules``.
 
 * Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 
