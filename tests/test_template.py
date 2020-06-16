55f0c791906b14392768834164c7c527b86ac500
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-16 07:28:32 -0400

    test: add default tests and template variables

--- tests/test_template.py
+++ tests/test_template.py
@@ -7,11 +7,24 @@ from unittest import TestCase
 from cookiecutter import main
 
 
+#: Define absolute path to cc-pydata cookiecutter project directory
 CCDIR = Path(__file__).resolve().parents[1]
 
 
+#: Define list of top-level files expected in built template
+template_files = [
+
+]
+
+
+#: Define list of sub-directories expected in built template
+template_directories = [
+
+]
+
+
 class TestBuildTemplate(TestCase):
-    """Test basic cookiecutter template build"""
+    """Test default cookiecutter template build"""
 
     def setUp(self):
         """Build template in temporary directory"""
@@ -35,3 +48,15 @@ class TestBuildTemplate(TestCase):
     def test_project_exists(self):
         """Ensure built template exists in temp dir"""
         self.assertTrue(os.path.isdir(self.builtdir))
+
+    def test_unfilled_brackets(self):
+        """Ensure no curly brackets are left over from jinja build"""
+        raise NotImplementedError
+
+    def test_files_exist():
+        """Ensure top-level files exist"""
+        raise NotImplementedError
+
+    def test_subdirs_exist():
+        """Ensure top-level sub-directories exist"""
+        raise NotImplementedError
