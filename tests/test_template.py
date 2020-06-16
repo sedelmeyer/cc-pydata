a8d1cff1dd44e566c29f3cc748fbc3c8cc5e12f3
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-16 06:58:36 -0400

    test(setup): add template build setup to TestCase

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,9 +1,37 @@
-
+import contextlib
+import os
 from pathlib import Path
+import tempfile
 from unittest import TestCase
 
 from cookiecutter import main
 
 
-def test_tests():
-    pass
+CCDIR = Path(__file__).resolve().parents[1]
+
+
+class TestBuildTemplate(TestCase):
+    """Test basic cookiecutter template build"""
+
+    def setUp(self):
+        """Build template in temporary directory"""
+        with contextlib.ExitStack() as stack:
+            # open temp directory context manager
+            self.tmpdir = tmpdir = stack.enter_context(
+                tempfile.TemporaryDirectory()
+            )
+            # build cookie template in temp directory
+            main.cookiecutter(
+                template=str(CCDIR),
+                no_input=True,
+                extra_context=None,
+                output_dir=tmpdir
+            )
+            # get path to built template directory
+            self.builtdir = Path(tmpdir).resolve() / 'project_name'
+            # ensure context manager closes after tests
+            self.addCleanup(stack.pop_all().close)
+
+    def test_project_exists(self):
+        """Ensure built template exists in temp dir"""
+        self.assertTrue(os.path.isdir(self.builtdir))
