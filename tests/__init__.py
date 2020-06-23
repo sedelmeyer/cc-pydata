68e78a3ea2d8e8ae863577d8b7a233129a19e961
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-22 20:38:23 -0400

    test: add find_jinja_brackets function

--- tests/__init__.py
+++ tests/__init__.py
@@ -42,8 +42,18 @@ def bake_cookiecutter_template(
 
 
 def find_jinja_brackets(input, regex=JINJA_REGEX):
+    """Find all instances of input string that contains jinja brackets
+
+    :param input: text within which to search for jinja brackets
+    :type input: str
+    :param regex: regular expression pattern, defaults to JINJA_REGEX
+    :type regex: str, optional
+    :return: list of string instances that match regex
+    :rtype: list
     """
-    """
+    regex_compiled = re.compile(regex)
+    result = regex_compiled.findall(input)
+    return result
 
 
 @contextlib.contextmanager
