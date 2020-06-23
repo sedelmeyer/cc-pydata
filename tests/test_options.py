a2b3cc7ca0bfdd22c5b7ddad07de58496feff3dd
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-23 12:55:18 -0400

    test: add test_not_open_source_license_option test

--- tests/test_options.py
+++ tests/test_options.py
@@ -58,22 +58,43 @@ class TestBuildTemplateOption(TestCase):
                 with open(licpath, 'r') as lic, open(setuppath, 'r') as setup:
                     license_text = lic.read()
                     setup_text = setup.read()
-                    # confirm that correct license name is listed in each file
-                    self.assertTrue(
-                        license_name.lower() in license_text.lower()
-                    )
-                    self.assertTrue(
-                        license_name.lower() in setup_text.lower()
-                    )
-                    # confirm that neither file contains unrendered jinja
-                    lic_jinja = tests.find_jinja_brackets(license_text)
-                    setup_jinja = tests.find_jinja_brackets(setup_text)
-                    self.assertEqual(len(lic_jinja), 0)
-                    self.assertEqual(len(setup_jinja), 0)
+                # confirm that correct license name is listed in each file
+                self.assertTrue(
+                    license_name.lower() in license_text.lower()
+                )
+                self.assertTrue(
+                    license_name.lower() in setup_text.lower()
+                )
+                # confirm that neither file contains unrendered jinja
+                lic_jinja = tests.find_jinja_brackets(license_text)
+                setup_jinja = tests.find_jinja_brackets(setup_text)
+                self.assertEqual(len(lic_jinja), 0)
+                self.assertEqual(len(setup_jinja), 0)
 
     def test_not_open_source_license_option(self):
-        """Ensure non-open source license option builds"""
-        raise NotImplementedError
+        """Ensure non-open source license option builds correctly"""
+        tests.bake_cookiecutter_template(
+            output_dir=self.tmpdir,
+            extra_context={
+                'license': license_list[-1]
+            }
+        )
+        builtdir = os.path.join(self.tmpdir, project_name)
+        # confirm post_gen_project hook removes LICENSE file
+        self.assertFalse(
+            os.path.exists(os.path.join(builtdir, 'LICENSE'))
+        )
+        # confirm setup.py generates correctly
+        setuppath = os.path.join(builtdir, 'setup.py')
+        with open(setuppath, 'r') as setup:
+            setup_text = setup.read()
+        # confirm that no license classifier is listed
+        self.assertTrue(
+            'License ::' not in setup_text
+        )
+        # confirm that file does not contain unrendered jinja
+        setup_jinja = tests.find_jinja_brackets(setup_text)
+        self.assertEqual(len(setup_jinja), 0)
 
     def test_travis_option_yes(self):
         """Ensure travis option builds"""
