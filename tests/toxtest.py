2608bba45e5d74672a2a86038f029a99e9c43165
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-26 07:21:58 -0400

    test: add extra_context and module docstring to tests.toxtest

--- tests/toxtest.py
+++ tests/toxtest.py
@@ -1,3 +1,35 @@
+"""
+tests.toxtest
+~~~~~~~~~~~~~
+
+This module contains tests to ensure the ``cc-pydata`` template's tox tests
+run wihtout error.
+
+This is accomplished by rendering the ``cc-pydata`` template to a temporary
+directory, then running that template's default ``tox`` configuration to
+ensure all ``tox`` environment tests pass without error.
+
+.. warning::
+
+   Tests contained in this module can take considerable time to run and will,
+   by default, be ignored when the ``pytest`` test-runner command is invoked.
+
+   * This "ignore" behavior is the result of the naming convention used for
+     this module
+
+In order to run tests in this module, this module will need to be explicitly
+specified while invoking the ``pytest`` command.
+
+For example, to run tests in this module, run:
+
+.. code-block:: Bash
+
+   pytest -s tests/toxtest.py
+
+The ``-s`` argument will disable all ``stdout`` capturing by ``pytest``,
+ensuring that the underlying ``tox`` processes are made visible in ``stdout``
+while ``tox`` runs.
+"""
 import contextlib
 import shlex
 import subprocess
@@ -18,7 +50,10 @@ class TestTemplateToxConfig(TestCase):
                 tempfile.TemporaryDirectory()
             )
             # build cookie template in temp directory
-            self.builtdir = tests.bake_cookiecutter_template(output_dir=tmpdir)
+            self.builtdir = tests.bake_cookiecutter_template(
+                output_dir=tmpdir,
+                extra_context={'tox': 'yes'}
+            )
             # ensure context manager closes after tests
             self.addCleanup(stack.pop_all().close)
 
@@ -27,7 +62,7 @@ class TestTemplateToxConfig(TestCase):
         with tests.working_directory(self.builtdir):
             # run 'git init' so that scm_setuptools versioning works
             subprocess.call(shlex.split('git init'))
-            # check that setup.py will return version
+            # check that template tox runs wihtout error
             result = subprocess.check_call(
                 shlex.split('tox')
             )
