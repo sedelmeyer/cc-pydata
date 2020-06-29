e40ff78a9abebb5ee024dcbbca15ad7750fec7e2
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-06-29 19:49:56 -0400

    test: proofread and make edits to toxtest docstrings

--- tests/toxtest.py
+++ tests/toxtest.py
@@ -2,8 +2,8 @@
 tests.toxtest
 ~~~~~~~~~~~~~
 
-This module contains tests to ensure the ``cc-pydata`` template's tox tests
-run wihtout error.
+This module contains tests to ensure the ``cc-pydata`` template's ``tox`` tests
+run without error.
 
 This is accomplished by rendering the ``cc-pydata`` template to a temporary
 directory, then running that template's default ``tox`` configuration to
@@ -14,21 +14,21 @@ ensure all ``tox`` environment tests pass without error.
    Tests contained in this module can take considerable time to run and will,
    by default, be ignored when the ``pytest`` test-runner command is invoked.
 
-   * This "ignore" behavior is the result of the naming convention used for
-     this module
+      * This "ignore" behavior is the result of the naming convention used for
+        this module.
 
-In order to run tests in this module, this module will need to be explicitly
-specified while invoking the ``pytest`` command.
+In order to run the tests contained in this module, this module will need to
+be explicitly specified while invoking the ``pytest`` command.
 
-For example, to run tests in this module, run:
+For example, use this command to run tests contained in this module:
 
 .. code-block:: Bash
 
    pytest -s tests/toxtest.py
 
-The ``-s`` argument will disable all ``stdout`` capturing by ``pytest``,
-ensuring that the underlying ``tox`` processes are made visible in ``stdout``
-while ``tox`` runs.
+The ``-s`` option will disable all ``stdout`` capturing by ``pytest``, ensuring
+that the underlying ``tox`` processes are made visible in ``stdout`` while
+the template's ``tox`` script runs.
 """
 import contextlib
 import shlex
@@ -40,10 +40,10 @@ import tests
 
 
 class TestTemplateToxConfig(TestCase):
-    """Test that template's default tox configuration runs without error"""
+    """Test that the rendered template's default ``tox`` configuration runs"""
 
     def setUp(self):
-        """Build template in temporary directory"""
+        """Render the ``cc-pydata`` template in a temporary directory"""
         with contextlib.ExitStack() as stack:
             # open temp directory context manager
             self.tmpdir = tmpdir = stack.enter_context(
@@ -58,7 +58,7 @@ class TestTemplateToxConfig(TestCase):
             self.addCleanup(stack.pop_all().close)
 
     def test_template_tox_runs(self):
-        """Ensure tox runs for default template"""
+        """Ensure ``tox`` runs without error for the rendered template"""
         with tests.working_directory(self.builtdir):
             # run 'git init' so that scm_setuptools versioning works
             subprocess.call(shlex.split('git init'))
